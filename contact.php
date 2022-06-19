685c9f568ac32e5b566793ef9bf77dd2b4e85b7c
ak1803 <jakshay18397@gmail.com>
2022-06-19 09:15:09 +0530

    alpha-mam

---
+++ contact.php
@@ -0,0 +1,391 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="font-awesome-4.7.0/css/font-awesome.min.css">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=My+Soul&display=swap" rel="stylesheet">
+    <title>Alpha Ortho & Skin Hospital</title>
+
+    <link
+    rel="stylesheet"
+    href="https://unpkg.com/swiper/swiper-bundle.min.css"
+  />
+  </head>
+  <body>
+
+
+
+    <div class="breadcrum">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-7">
+                   <div class=social>
+                   <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
+                   <a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+                   <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+                   </div> 
+                </div>
+                <div class="col-md-5 d-lg-flex">
+                  <p class="whats-app"><img src="images/whatsapp.png" alt=""> WhatsApp</p>
+                <a href="tel:7721018961">   <p class="call"><img src="images/phone-call.png" alt=""> +91-7721018961</p></a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+   <div class="navi">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12">
+            <nav class="navbar navbar-expand-lg ">
+              <a class="navbar-brand" href="index.html"><img src="images/logo.png" alt=""></a>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"><img src="images/hamburger.png" alt=""> </span>
+              </button>
+            
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav ml-auto">
+                  <li class="nav-item active">
+                    <a class="nav-link" href="index.html">Home <span class="sr-only">(current)</span></a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="about.html">About</a>
+                  </li>
+                  <li class="nav-item dropdown">  
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-expanded="false">
+                      Services
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                      <div class="col-md-4 float-lg-left">
+                        <div class="box">
+                          <h2>Skin Treatment</h2>
+                        </div>
+                      
+                        <div class="sub-menu">
+                          <a href="acne.html">Acne</a>
+                          <a href="mole.html">Mole Removal</a>
+                          <a href="scars.html">Scars Removal</a>
+                          <a href="wart.html">Wart Removal</a>
+                          <a href="Pigmentation.html">Pigmentation</a>
+                          <a href="Psoriasis.html">Psoriasis</a>
+                          <a href="Pediatric.html">Pediatric </a>
+                          <a href="Lipoma.html">Lipoma </a>
+                          <a href="services.html">Other Services </a>
+                        </div>
+
+                        <div class="box mt-4">
+                          <h2>Anti Aging</h2>
+                        </div>
+
+                        <div class="sub-menu">
+                          <a href="#">Wrinkle Treatment</a>
+                          <a href="#">Dermal Fillers</a>
+                          <a href="#">Vampire Facial</a>
+                          <a href="#">Ultherapy Lift</a>
+                          <a href="#">Sublime® RF DermaLift</a>
+                          <a href="#">Thread Lift</a>
+                        </div>
+
+                      </div>
+                      <div class="col-md-4 float-lg-left">
+                        <div class="box">
+                          <h2>Hair Treatment</h2>
+                        </div>
+                   
+                        <div class="sub-menu">
+                          <a href="Hair-fall.html">Laser Hair Removal</a>
+                          <a href="mesotherapy.html">Mesotherapy</a>
+                          <a href="prp-hair.html">PRP Hair Treatment</a>
+                          <a href="transplant.html">Hair Transplant</a>
+                        </div>
+
+                        <div class="box mt-4">
+                          <h2>Products</h2>
+                        </div>
+
+                        <div class="sub-menu">
+                          <a href="#">Skin care Home product</a>
+                          <a href="#">Paird Supplements</a>
+                        </div>
+
+
+                      </div>
+                      <div class="col-md-4 float-lg-left">
+                        <div class="box">
+                          <h2>Body Treatment</h2>
+                        </div>
+                    
+                        <div class="sub-menu">
+                          <a href="#">Laser Hair Removal</a>
+                          <a href="#">Bridal Skincare</a>
+                          <a href="#"> IV Drips & Detox Therapy</a>
+                         
+                        </div>
+
+                     <div class="box mt-4">
+                          <h2>Dermato Surgery</h2>
+                        </div>
+
+                        <div class="sub-menu">
+                          <a href="#">Dermatosurgery </a>
+                          <a href="#">Lypo surgery  </a>
+                          <a href="#">blepharoplasty(eye lid lift) </a>
+                          <a href="#">Vitiligo surgery  </a>
+                          <a href="#">Nail surgery </a>
+                          <a href="#">Cryo surgery </a>
+                          <a href="#">Electro surgery</a>
+                          <a href="#">Skin biopsy</a>
+                        </div>
+
+
+                      </div>
+                    </div> 
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="gallery.html">Gallery</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="blog.html">Blog</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link" href="contact.php">Contact</a>
+                  </li>
+                </ul>
+               <button type="button" class="offer-btn ml-lg-4">Offer</button>
+              </div>
+            </nav>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  
+
+
+<div class="head">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <h2>CONTACT US</h2>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+<div class="form">
+    <div class="container">
+      <div class="row"> 
+        <div class="contact-box">
+          <div class="container">
+              <div class="row">
+                  <div class="col-md-6 col-lg-4 aos-item" data-aos="fade-right" data-aos-duration="2000">  
+                    <h4>Our Location</h4>
+                      <div class="bg">
+                        <p class="add">First Floor, C Wing, RK Life Space, Near Preet’s Punjabi Swad Hotel, Datta Mandir Rd, Shankar Kalat Nagar, Wakad, Pune, Maharashtra 411057</p>
+                      <a href="tel:9096291478"> <p class="call">+91-90962 91478 / +91-82757 36522</p></a>
+                       <a href="#"> <p class="mail">xyz@gmail.com</p></a> 
+                       
+                      </div>
+                  </div>
+                  <div class="col-md-6 col-lg-8 aos-item" data-aos="fade-left" data-aos-duration="2000">
+                    <h4>Get in touch</h4>
+                      <div class="form-box">
+                      <form id="contact_us_frm" action='action.php' method="post" >
+                      <input type="hidden" name="action" value="contact_us_form">
+                           <div class="col-md-6 pl-lg-0 pr-lg-0 float">
+                              <input type="text" class="form-control" name="fname" id="fname" placeholder="Full Name">
+                              <small id="firstname_error" class="text-danger error_msg d-none">This Field is Required</small>
+                           </div>
+  
+                           <div class="col-md-6 pr-lg-0 float">
+                            <input type="text" class="form-control" name="lname" id="lname" placeholder="Last Name">
+                            <small id="lastname_error" class="text-danger error_msg d-none">This Field is Required</small>
+                           </div>
+  
+                           <div class="col-md-6 pl-lg-0 pr-lg-0 float">
+                            <input type="text" class="form-control" name="mobile" id="mobile" placeholder="Phone">
+                            <small id="mobile_error" class="text-danger error_msg d-none">This Field is Required</small>
+                           </div>
+  
+                           <div class="col-md-6 pr-lg-0 float">
+                            <input type="email" class="form-control" name="email" id="email" placeholder="E-mail">
+                            <small id="email_error" class="text-danger error_msg d-none">This Field is Required</small>
+                           </div>
+  
+                           <div class="col-md-12 pl-lg-0 pr-lg-0 float">
+                           <textarea class="form-control" placeholder="Message" name="message" id="message"></textarea>
+                           <small id="message_error" class="text-danger error_msg d-none">this Field is Required</small>
+                           </div>
+  
+                           <div class="clearfix"></div>
+                           
+                           <div class="col-md-12 pl-lg-0 pr-lg-0">
+                             <button type="button" value="Submit" id="submit_contact_form" class="send-btn">Send now</button>
+                           </div>
+                      </div>
+                  </div>
+
+                  <div class="col-md-12">
+                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3781.4581784837405!2d73.7619555153089!3d18.598450571640946!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bc2b96e93942ad9%3A0x47650020073af1b4!2sAlpha%20Ortho%20%26%20Skin%20Hospital!5e0!3m2!1sen!2sin!4v1648549109129!5m2!1sen!2sin"  width="100%" height="400" style="border:0; box-shadow: rgba(149, 157, 165, 0.2) 0px 8px 24px; border-radius: 15px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                </div>
+              </div>
+          </div>
+      
+      </div>
+    </div>
+    </div>
+  </div>
+
+
+
+
+
+
+<div class="intrest">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-7">
+          <h2>Interested in consulting with Alpha Hospital ?</h2>
+      </div>
+      <div class="col-md-5 mt-lg-4">
+        <button type="button" class="btn-int mr-lg-3"><i class="fa fa-whatsapp" aria-hidden="true"></i>&nbsp;Whats App</button>
+    <a href="contact.php"><button type="button" class="btn-int"><i class="fa fa-calendar" aria-hidden="true"></i>&nbsp; Appointment</button></a>
+      </div>
+    </div>
+  </div>
+</div>
+
+
+
+
+<div class="footer">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3">
+        <h3>SKIN TREATMENT</h3>
+        <ul>
+          <a href="acne.html"><li> Acne</li></a>
+          <a href="mole.html"><li> Mole Removal</li></a>
+          <a href="scars.html"><li>Scars Removal</li></a>
+          <a href="wart.html"><li>Wart Removal </li></a>
+          <a href="Pigmentation.html"><li>Pigmentation</li></a>
+          <a href="Psoriasis.html"><li>Psoriasis</li></a>
+          <a href="Pediatric.html"><li>Pediatric</li> </a>
+          <a href="Lipoma.html"><li>Lipoma </li></a>
+          <a href="services.html"><li>Other Services</li> </a>
+        </ul>
+      </div>
+      <div class="col-md-3">
+        <h3>YOUTH PRESERVE</h3>
+        <ul>
+          <a href="#"><li>Wrinkle Treatment</li></a>
+          <a href="#"><li>Dermal Fillers</li></a>
+          <a href="#"><li>Vampire Facial </li></a>
+          <a href="#"><li>Ultherapy Lift</li></a>
+          <a href="#"><li>Sublime® RF DermaLift</li></a>
+          <a href="#"><li>Thread Lift</li></a>
+        </ul>
+      </div>
+      <div class="col-md-3">
+
+        <h3>FAT REDUCTION & BODYSCULPTING</h3>
+        <ul>
+          <a href="#"><li>Coolsculpting</li></a>
+          <a href="#"><li>Emsculpt</li></a>
+          <a href="#"><li>Thermage Skin Tightening</li></a>
+        </ul>
+
+        <h3 class="mt-4">HAIR RESTORE</h3>
+        <ul>
+          <a href="Hair-fall.html"><li> Laser Hair Removal</li></a>
+          <a href="mesotherapy.html"><li>Mesotherapy</li></a>
+          <a href="prp-hair.html"><li>PRP Hair Treatment</li></a>
+          <a href="transplant.html"><li>Hair Transplant</li></a>
+        </ul>
+
+      </div>
+      <div class="col-md-3">
+       
+        <h3>BODY TREATMENT</h3>
+        <ul>
+          <a href="#"><li>Laser Hair Removal</li></a>
+          <a href="#"><li>Bridal Skincare</li></a>
+          <a href="#"><li>IV Drips & Detox Therapy</li></a>
+        </ul>
+
+        <h3 class="mt-5">PRODUCT</h3>
+        <ul>
+          <a href="#"><li>Home Care</li></a>
+          <a href="#"><li>Paird Supplements</li></a>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+
+
+  <div class="container">
+    <div class="row">
+      <div class="col-md-12">
+        <ul class="links">
+          <li><a href="index.html">Home</a></li>
+          <li><a href="about.html">About</a></li>
+          <li><a href="services.html">Services</a></li>
+          <li><a href="gallery.html">Gallery</a></li>
+          <li><a href="blog.html">Blog</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </div>
+    </div>
+  </div>  
+
+
+  
+  <div class="container">
+    <div class="row">
+      <div class="col-md-6">
+   <p class="copy">© 2022 Copyright. All rights reserved</p>
+      </div>
+
+      <div class="col-md-6">
+        <div class="social">
+          <a href="#"> <i class="fa fa-facebook" aria-hidden="true"></i></a>
+          <a href="#"> <i class="fa fa-twitter" aria-hidden="true"></i></a>
+          <a href="#"><i class="fa fa-instagram" aria-hidden="true"></i></a>
+        </div>
+      </div>
+    </div>
+  </div> 
+
+</div>
+
+
+
+
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.min.js" integrity="sha384-VHvPCCyXqtD5DqJeNxl2dtTyhF78xXNXdkwX1CZeRusQfRKp+tA7hAShOK/B/fQ2" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
